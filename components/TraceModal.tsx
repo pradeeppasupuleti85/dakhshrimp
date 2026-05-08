463fb923f92a892e8f02a1cc3624c8c6031e285c
pradeeppasupuleti85 <pradeeppasupuleti85@gmail.com>
2026-05-08 12:04:26 +0530

    Change & update TraceModel

--- components/TraceModal.tsx
+++ components/TraceModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import { BatchData } from "@/lib/batchData";
+import { BatchData } from "@/lib/types";
 
 interface Props {
   batch: BatchData;
